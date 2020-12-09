a0249eb63ad72a54c7b14d55b5316b06ec71ab30
Deborah Martin <debomart@cisco.com>
2020-12-09 03:22:59 -0800

    Tidy up and slight speed up 9 now ~100ms

--- 9/9.py
+++ 9/9.py
@@ -2,13 +2,14 @@
 
 import itertools
 
+PREAMBLE_LEN=25
+
 def find_weakness(invalid):
     for i in range(invalid): 
         curr_sum = nums[i] 
       
         # try all subarrays starting at i
-        j = i + 1
-        while j <= invalid: 
+        for j in range(i + 1, invalid): 
             if curr_sum == nums[invalid]: 
                 numsub = nums[i:j]
                 return min(numsub) + max(numsub)
@@ -16,20 +17,40 @@ def find_weakness(invalid):
             if curr_sum > nums[invalid] or j == invalid: 
                 break
               
-            curr_sum = curr_sum + nums[j] 
-            j += 1
+            curr_sum = curr_sum + nums[j]
 
-PREAMBLE_LEN=25
+# Tried doing this to avoid computing permutations of all PREAMBLE_LEN each 
+# time but was actually slower... 
+#
+# def find_invalid_index():
+#    pairs = [[nums[pair[0]], nums[pair[1]]] for pair in itertools.permutations(range(0,PREAMBLE_LEN), 2)]
+#    for i in range(PREAMBLE_LEN, len(nums)):
+#        if nums[i] not in set(map(lambda x: sum(x), pairs)):
+#            return i
+#
+#        # take the i - PREAMBLE_LEN one out of the pairs and add in i
+#        # NOTE: this assumes no repeats in subarray
+#        for pair in pairs:
+#            if nums[i - PREAMBLE_LEN] in pair:
+#                pair.remove(nums[i - PREAMBLE_LEN])
+#                pair.append(nums[i])
+
+def find_invalid_index():
+    for i in range(PREAMBLE_LEN, len(nums)):
+        sums_pairs = (
+            sum(numvars) for numvars in 
+            itertools.permutations(nums[i - PREAMBLE_LEN:i], 2)
+        )
+ 
+        if nums[i] not in sums_pairs:
+            return i
+
 
 with open("9/input.txt", "r") as f:
     nums = list(map(lambda x: int(x), f.read().split("\n")))
 
 print("PART 1:")
-for i in range(PREAMBLE_LEN, len(nums)):
-    sums_pairs = [sum(numvars) for numvars in itertools.permutations(nums[i-PREAMBLE_LEN:i], 2)] #@@@ better way to do this
-    if nums[i] not in sums_pairs:
-        invalid = i
-        break
+invalid = find_invalid_index()
 print(nums[invalid])
 
 # find contiguous set which add to number
