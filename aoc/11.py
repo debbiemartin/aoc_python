ec2ab1f17b868e692b826b28c8511b2f2ecc67c6
Deborah Martin <debomart@cisco.com>
2020-12-11 15:08:04 -0800

    Down to 1s..

--- aoc/11.py
+++ aoc/11.py
@@ -2,25 +2,49 @@
 
 from . import utils
 import itertools
+from collections import namedtuple
 
+class Seat(object):
+    def __init__(self):
+        self.n_adj = 0
+        self.filled = False
+        self.neighbours = []
+    
+    def add_neighbour(self, neighbour):
+        self.neighbours.append(neighbour)
 class SeatSim(object):
     """
     Seat simulator. Iterates the seat map in time intervals. 
     """
        
-    def __init__(self):
+    def __init__(self, part2=False):
         self.seatmap = {}
+        self.part2 = part2
 
         lines = utils.get_lines(11)
         self.YMAX = len(lines)
         self.XMAX = len(lines[0])
-        for y, line in enumerate(lines):
-            for x in range(len(line)):
-                if line[x] == "#":
-                    self.seatmap[(x, y)] = True
-                elif line[x] == "L":
-                    self.seatmap[(x, y)] = False
+        for x, y in ((x, y) for y, line in enumerate(lines) for x in range(len(line)) if line[x] == "L"):
+            self.seatmap[(x, y)] = Seat()
         self.neighbour_incrs = set(itertools.permutations([-1,-1,0,1,1], 2))
+        
+        # Work out each seat's neighbours up front
+        for coord, seat in self.seatmap.items():
+            if part2:
+                for n in self.neighbour_incrs:
+                    # find line of sights - make sure we don't leave the grid
+                    ncoord = coord
+                    while (ncoord[0] >= 0 and ncoord[0] <= self.XMAX and 
+                           ncoord[1] >= 0 and ncoord[1] <= self.YMAX):
+                        ncoord = self.add_coord(ncoord, n)
+                        if ncoord in self.seatmap:
+                            seat.add_neighbour(ncoord)
+                            break
+            else:
+                # Nearest neighbours
+                for neighbour in (self.add_coord(n, coord) for n in self.neighbour_incrs):
+                    if neighbour in self.seatmap:
+                        seat.add_neighbour(neighbour)
     
     def __str__(self):
         str = ""
@@ -28,7 +52,7 @@ class SeatSim(object):
             for x in range(self.XMAX):
                 str += (
                     "." if (x,y) not in self.seatmap else 
-                    ("#" if self.seatmap[(x,y)] else "L")
+                    ("#" if self.seatmap[(x,y)].filled else "L")
                 )
             str += "\n"
 
@@ -37,51 +61,44 @@ class SeatSim(object):
     def add_coord(self, coord1, coord2):
         return (coord1[0] + coord2[0], coord1[1] + coord2[1])
         
-    def _iterate(self, part2):
+    def _iterate(self):
         changes = []
+        
+        #@@@ could pass through an array to check - all first time then
+        #    ones who've had n_adj changed the second
         for y in range(self.YMAX):
             for x in range(self.XMAX):
                 # don't process floor space
                 if (x,y) not in self.seatmap:
                     continue
-           
-                if not part2:
-                    n_adj = sum(
-                        1 for n in self.neighbour_incrs if 
-                        self.add_coord(n, (x,y)) in self.seatmap and self.seatmap[self.add_coord(n, (x,y))]
-                    )
-                else:
-                    n_adj = 0
-                    for n in self.neighbour_incrs:
-                        # go in inc until 
-                        coord = (x,y)
-                        while (coord[0] >= 0 and coord[0] <= self.XMAX and 
-                               coord[1] >= 0 and coord[1] <= self.YMAX):
-                            coord = self.add_coord(coord, n)
-                            if coord in self.seatmap:
-                                if self.seatmap[coord]:
-                                    n_adj += 1
-                                break
+                
+                seat = self.seatmap[(x,y)]
                 
                 # Empty or fill seat depending on number adjacent
-                if ((self.seatmap[(x,y)] and 
-                    (n_adj >= 5 or n_adj == 4 and not part2)) or 
-                    (not self.seatmap[(x,y)] and n_adj == 0)):
+                if ((seat.filled and 
+                    (seat.n_adj >= 5 or seat.n_adj == 4 and not self.part2)) or 
+                    (not seat.filled and seat.n_adj == 0)):
                     changes.append((x,y))
         
         # Apply the changes to seatmap
         for coord in changes:
-            self.seatmap[coord] = not self.seatmap[coord]
-         
+            seat = self.seatmap[coord]
+            seat.filled = not seat.filled
+            
+            for neighbour in seat.neighbours:
+                self.seatmap[neighbour].n_adj += (1 if seat.filled else -1)
+
         return len(changes) != 0
                
-    def iterate_until_static(self, part2=False):
+    def iterate_until_static(self):
         while True:
             #print(self) # for debug
-            if not self._iterate(part2):
+            if not self._iterate():
                 break
         
-        return sum(1 for seat in self.seatmap.values() if seat)
+        return sum(1 for seat in self.seatmap.values() if seat.filled)
+    
+    #@@@ reset function instead of making fresh class for part 2?
 
 def main():  
     s = SeatSim()
@@ -89,8 +106,8 @@ def main():
     print("PART 1:")
     print(s.iterate_until_static())
     
-    s = SeatSim()
+    s = SeatSim(True)
     
     print("PART 2:")
-    print(s.iterate_until_static(True))
+    print(s.iterate_until_static())
     
