6737e059ff5e18e2f4e4107a188ae7cf1af9389f
Deborah Martin <debomart@cisco.com>
2020-12-20 13:06:40 -0800

    non general solution for day 19

--- aoc/19.py
+++ aoc/19.py
@@ -15,13 +15,35 @@ def parse(lines):
     
     return rules, expanded
 
+
+def matches_rules(test, index, rules):
+    if (index == len(test)):
+        return 0, index
+    
+    for rule in (rule for rule in rules if test[index:].startswith(rule)):
+        count, newindex = matches_rules(test, index + len(rule), rules)
+        return count + 1, newindex
+    
+    return 0, index
+   
+def matches_rulelists(test, rules1, rules2):
+    """
+    Used in part 2 to match minimum 2 of 42 rules, then minimum 1 31 rule. 
+    """
+    n, index = matches_rules(test, 0, rules1)
+    m, index = matches_rules(test, index, rules2)
+    if index != len(test) or m < 1 or n < (m + 1):
+        return False
+    
+    return True
+
 def main():
     sections = utils.get_sections(19)
     rules, expanded = parse(sections[0].split("\n"))
     
     while len(expanded) != len(rules):
         for i, rule in [(i, rule) for (i, rule) in rules.items() if i not in expanded]:
-            if all(i in expanded for subrule in rule for i in subrule):
+            if all(x in expanded for subrule in rule for x in subrule if x != i):
                 rules[i] = [
                     "".join(i)
                     for subrule in rule
@@ -31,4 +53,11 @@ def main():
     
     print("PART 1:")
     print(len(set(rules[0]) & set(sections[1].split("\n"))))
+    
+    print("PART 2:")
+    # New rules effectively match 42 n times and 31 m times - just cheat and
+    # check for this rather than evaluating loops intelligently
+    #import pdb; pdb.set_trace()
+    print(sum(1 for test in sections[1].split("\n") if matches_rulelists(test, rules[42], rules[31])))
+                                
             
