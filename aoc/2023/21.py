13321d6cb6cd30380bce77a7359f2af36ad295d9
Debbie Martin <Debbie.Martin@arm.com>
2023-12-21 11:48:17 +0000

    2023 day 21 improvement

--- aoc/2023/21.py
+++ aoc/2023/21.py
@@ -1,4 +1,5 @@
 from collections import deque
+import numpy as np
 from .. import utils
 
 class Day(object):
@@ -9,7 +10,7 @@ class Day(object):
         self.rocks = set()
         lines = self.parser.get_lines()
         assert len(lines) == len(lines[0])
-        self.max = len(lines)
+        self.grid_size = len(lines)
 
         for y, line in enumerate(lines):
             for x, char in enumerate(line):
@@ -18,61 +19,34 @@ class Day(object):
                 if char == "S":
                     self.start = (x,y)
 
-        assert self.start[0] == self.max // 2
-        assert self.start[1] == self.max // 2
+    def solve(self, steps):
+        modulos = []
+        coords = set([self.start])
 
-    def next_coords(self, coord):
-        for direction in [(0,1), (1,0), (0,-1), (-1,0)]:
-            ncoord = utils.add_coord(coord, direction)
-            if ncoord in self.rocks:
-                continue
-            if ncoord[0] < 0 or ncoord[0] >= self.max or ncoord[1] < 0 or ncoord[1] >= self.max:
-                continue
-            yield ncoord
+        for i in range(1, 1 + steps):
+            new_coords = set()
+            for coord in coords:
+                for direction in ((0,1), (1,0), (0,-1), (-1,0)):
+                    ncoord = utils.add_coord(coord, direction)
+                    if not (ncoord[0]%self.grid_size, ncoord[1]%self.grid_size) in self.rocks:
+                        new_coords.add(ncoord)
+            coords = new_coords
 
-    def fill(self, start, steps):
-        final = set()
-        seen = set([start])
-        queue = deque([(start, steps)])
+            if i % self.grid_size == steps % self.grid_size:
+                modulos.append(len(coords))
 
-        while queue:
-            coord, n_steps = queue.popleft()
+            if len(modulos) == 3:
+                x = np.array([0,1,2])
+                y = np.array(modulos)
+                poly = np.polyfit(x, y, deg=2)
+                return np.polyval(poly, steps // self.grid_size)
 
-            if n_steps % 2 == 0:
-                final.add(coord)
-            if n_steps == 0:
-                continue
-
-            for next_coord in self.next_coords(coord):
-                if not next_coord in seen:
-                    seen.add(next_coord)
-                    queue.append((next_coord, n_steps-1))
-
-        return len(final)
+        return len(coords)
 
     def part_1(self):
-        return self.fill(self.start, 64)
+        return self.solve(64)
 
     def part_2(self):
-        steps = 26501365
-        assert steps % self.max == self.max // 2
+        #return test(self.parser.get_lines(), 26501365)
+        return self.solve(26501365)
 
-        max_inf = steps // self.max - 1
-
-        odd_points = self.fill(self.start, self.max * 2 + 1)
-        even_points = self.fill(self.start, self.max * 2)
-
-        corners = (self.fill(c, self.max-1) for c in ((self.start[0], 0), (self.start[0], self.max-1), (0, self.start[1]), (self.max-1, self.start[1])))
-
-        smalls = (self.fill(c, self.max // 2 - 1) for c in ((0, 0), (0, self.max-1), (self.max-1, 0), (self.max-1, self.max-1)))
-        larges = (self.fill(c, self.max * 3 // 2 - 1) for c in ((0, 0), (0, self.max-1), (self.max-1, 0), (self.max-1, self.max-1)))
-
-        answer = (
-            ((max_inf // 2 * 2 + 1) ** 2) * odd_points +
-            (((max_inf + 1) // 2 * 2) ** 2) * even_points +
-            sum(corners) +
-            (max_inf + 1) * sum(smalls) +
-            (max_inf) * sum(larges)
-        )
-
-        return answer
